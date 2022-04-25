ec5aac086226dfeebcedc5ea865f007e2804daa8
linliquan <2641994740@qq.com>
2022-04-25 22:31:01 +0800

    修改HelloServlet.java

--- hello_maven/src/main/java/com/lagou/servlet/HelloServlet.java
+++ hello_maven/src/main/java/com/lagou/servlet/HelloServlet.java
@@ -12,6 +12,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("hello maven !!!");
+		System.out.println("hello maven !!!");
     }
 
     @Override
